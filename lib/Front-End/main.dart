8a5c036bcad922c1545a125b12372c3ed905f714
ashutos-h-tiwari <tiwariashutosh3884@gmail.com>
2025-09-09 22:34:22 +0530

    added login signup ui-frontend

--- lib/Front-End/main.dart
+++ lib/Front-End/main.dart
@@ -1,85 +1,18 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:dio/dio.dart';
+import 'Login-Signup/Login.dart';
 
-class MyApp extends StatefulWidget {
-  // ... as before
-  @override
-  State<MyApp> createState() => _MyHomePageState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyHomePageState extends State<MyApp> {
-  XFile? _selectedImage;
-  final ImagePicker _picker = ImagePicker();
-  String? _uploadStatus;
-
-  Future<void> _pickImage() async {
-    try {
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-      setState(() {
-        _selectedImage = image;
-        _uploadStatus = null;
-      });
-    } catch (e) {
-      setState(() {
-        _uploadStatus = 'Image pick error: $e';
-      });
-    }
-  }
-
-  Future<void> uploadImageToBackend(XFile imageFile) async {
-    try {
-      var dio = Dio();
-      String filename = imageFile.path.split('/').last;
-      var formData = FormData.fromMap({
-        "image": await MultipartFile.fromFile(imageFile.path, filename: filename),
-      });
-      Response response = await dio.post(
-        "https://your-backend-api-url.com/upload", // Use your real endpoint
-        data: formData,
-        options: Options(contentType: 'multipart/form-data'),
-      );
-      setState(() {
-        _uploadStatus = "Upload successful: ${response.statusCode}";
-      });
-    } catch (e) {
-      setState(() {
-        _uploadStatus = "Upload failed: $e";
-      });
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // ... rest of the UI
-      body: Center(
-        child: Column(
-          children: [
-            _selectedImage != null
-                ? Image.file(File(_selectedImage!.path), height: 200)
-                : Text('No image selected.'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Pick Image from Gallery'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _selectedImage != null
-                  ? () => uploadImageToBackend(_selectedImage!)
-                  : null,
-              child: Text('Send Image to Backend'),
-            ),
-            if (_uploadStatus != null)
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(_uploadStatus!, style: TextStyle(color: Colors.green)),
-              ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      title: 'Image Picker Demo',
+      home: LoginPage(),
     );
   }
 }
+
